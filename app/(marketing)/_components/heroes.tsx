c9d958b431a523086c5126d63eb5fb6286f32f01
0seo8 <00seo23@gmail.com>
2024-04-14 09:48:10 +0900

    fix: png파일 수정

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -6,10 +6,12 @@ export default function Heroes() {
     <div className="flex max-w-5xl flex-col items-center justify-center">
       <div className="flex items-center">
         <div className="relative size-[300px] sm:size-[350px] md:size-[400px]">
-          <Image alt="Documents" className="object-contain" fill src="/documents.webp" />
+          <Image alt="Documents" className="object-contain dark:hidden" fill src="/documents.png" />
+          <Image alt="Documents" className="hidden object-contain dark:block" fill src="/documents-dark.png" />
         </div>
         <div className="relative hidden size-[400px] md:block ">
-          <Image alt="Reading" className="object-contain" fill src="/reading.webp" />
+          <Image alt="Reading" className="object-contain dark:hidden" fill src="/reading.png" />
+          <Image alt="Reading" className="hidden object-contain dark:block" fill src="/reading-dark.png" />
         </div>
       </div>
     </div>
